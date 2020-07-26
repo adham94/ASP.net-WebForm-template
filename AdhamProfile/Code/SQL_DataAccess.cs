e5fc3967a5392aa106b167b4ff14882156cb5e94
Muhammad Adham in Boharim <muhd.adham94@gmail.com>
2020-07-26 12:20:43 +0800

    Add files via upload

---
+++ AdhamProfile/Code/SQL_DataAccess.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Ns_Constants;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Diagnostics;
+using System.Configuration;
+
+namespace Ns_DataAccess
+{
+    public class SQL_DataAccess
+    {
+        public static string ConnectionString()
+        {
+            string connstring = ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString;
+            return connstring;
+        }
+
+        public bool ExecuteSQL(string sSQL)
+        {
+            SqlConnection sqlConnection = null;
+            SqlCommand sqlCommand = null;
+            SqlTransaction sqlTransaction = null;
+            //sErr = "";
+            try
+            {
+                try
+                {
+                    sqlConnection = new SqlConnection()
+                    {
+                        ConnectionString = string.Concat(ConnectionString(), "Connect Timeout=30")
+                    };
+                    sqlConnection.Open();
+                    sqlTransaction = sqlConnection.BeginTransaction();
+                    sqlCommand = new SqlCommand(sSQL, sqlConnection, sqlTransaction);
+                    sqlCommand.ExecuteNonQuery();
+                    sqlTransaction.Commit();
+                }
+                catch (Exception)
+                {
+                    /*ProjectData.SetProjectError(exception1);
+					Exception exception = exception1;
+					sErr = string.Concat(exception.Source, " ", exception.Message);
+					num = 1;
+					ProjectData.ClearProjectError();
+					return num;*/
+                }
+            }
+            finally
+            {
+                sqlCommand.Dispose();
+                sqlConnection.Dispose();
+                sqlTransaction.Dispose();
+                sqlCommand = null;
+                sqlConnection = null;
+                sqlTransaction = null;
+            }
+
+            return true;
+        }
+
+        public DataSet GetDataSet(string sSQL)
+        {
+            SqlConnection cnn;
+            SqlDataAdapter dad;
+            DataSet dts = new DataSet();
+            cnn = new SqlConnection(ConnectionString());
+            dad = new SqlDataAdapter(sSQL, cnn);
+            try
+            {
+                cnn.Open();
+                dad.Fill(dts);
+                cnn.Close();
+
+                return dts;
+            }
+            catch (Exception)
+            {
+
+                return dts;
+            }
+            finally
+            {
+                dad.Dispose();
+                cnn.Dispose();
+                dts = null;
+                cnn = null;
+            }
+        }
+    }
+}
---
+++ AdhamProfile/Code/SQL_User.cs
@@ -0,0 +1,62 @@
+﻿using System.Data;
+using Ns_DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ns_SQL
+{
+    public class SQL_User_Object
+    {
+        protected string sFULLNAME = "";
+        protected string sEMAIL = "";
+        protected string sPASSWORD = "";
+        protected string sSTATUS = "";
+
+        public string FULLNAME   // property
+        {
+            get { return sFULLNAME; }   // get method
+            set { sFULLNAME = value; }  // set method
+        }
+        public string EMAIL   // property
+        {
+            get { return sEMAIL; }   // get method
+            set { sEMAIL = value; }  // set method
+        }
+        public string PASSWORD   // property
+        {
+            get { return sPASSWORD; }   // get method
+            set { sPASSWORD = value; }  // set method
+        }
+        public string STATUS   // property
+        {
+            get { return sSTATUS; }   // get method
+            set { sSTATUS = value; }  // set method
+        }
+        public class SQL_User_DA_Access
+        {
+            public bool InsertUser(SQL_User_Object obj_User)
+            {
+                string fullname = obj_User.FULLNAME;
+                string email = obj_User.EMAIL;
+                string password = obj_User.PASSWORD;
+                string status = "Active";
+
+                SQL_DataAccess SQLDataAccess = new SQL_DataAccess();
+
+                string sSQL = "SELECT * FROM user_info where email = '" + email + "';";
+
+                DataSet ds = SQLDataAccess.GetDataSet(sSQL);
+
+                if (ds.Tables[0].Rows.Count == 0)
+                {
+                    string sSQL2 = "INSERT INTO user_info (fullname,email,password,status) VALUES('" + fullname + "','" + email + "','" + password + "','" + status + "');";
+                    SQLDataAccess.ExecuteSQL(sSQL2);
+                    return true;
+                }
+                return true;
+            }
+        }
+    }
+}
